cc8ebef2becfcb7c6417301de25a50a98be0c145
Wiame-Bech <130190673+Wiame-Bech@users.noreply.github.com>
2024-05-14 15:14:57 +0200

    first commit

---
+++ SchoolOps/src/main/java/com/SchoolOps/SchoolOps/SchoolOpsApplication.java
@@ -0,0 +1,13 @@
+package com.SchoolOps.SchoolOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolOpsApplication.class, args);
+	}
+
+}
---
+++ SchoolOps/src/test/java/com/SchoolOps/SchoolOps/SchoolOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SchoolOps.SchoolOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
